7222c1fcfdb6d849d555516638e59fb14aa464d8
Ivet Popova <ivetpopova@Ivets-Laptop.local>
2025-02-02 22:01:48 +0200

    initial files added to repository

---
+++ main.py
@@ -0,0 +1,24 @@
+from math_functions import sumF, sq, sq_eq, avg, mediana, moda
+
+
+if __name__ == "__main__":
+    numbers = [1,7,10,10,5,-3,11,11,5]
+    a = 5
+    b = 4
+
+    s = sumF(a,b)
+    print("Sum: ", s)
+
+    r = sq(b)
+    print("Square: ", r)
+
+    res = sq_eq(a=1,b=2,c=-3)
+    print("Square equation", res)
+
+    r = avg(numbers)
+    print("Average: ", r)
+
+    Me = mediana(numbers)
+    print("Median: ", Me)
+
+    print("Mode: ",moda(numbers))
---
+++ math_functions.py
@@ -0,0 +1,48 @@
+import math
+
+
+def sumF(number1,number2):
+    return number1+number2
+
+def sq(num1):
+    return num1*num1
+
+def sq_eq(a,b,c):
+    D=sq(b)-(4*a*c)
+    if D>0:
+        x1=(-b+math.sqrt(D))/(2*a)
+        x2=(-b-math.sqrt(D))/(2*a)
+        return x1,x2
+    else:
+        x=-b/(2*a)  
+        return x
+
+def avg(numbersList):
+    res=sum(numbersList)/len(numbersList)
+    return res
+
+def mediana(numbers):
+    numbers.sort()
+    if len(numbers)%2==1:
+        pos=int((len(numbers)+1)/2)
+        return numbers[pos-1]
+    else:
+        pos=int(len(numbers)/2)
+        pos2=pos+1
+        return (numbers[pos-1]+numbers[pos2-1])/2
+
+def moda(numbers):
+    numDict={}
+    for i in range(len(numbers)):
+        currentNum=numbers[i]
+        if currentNum in numDict:
+            numDict[currentNum]+=1
+        else:
+            numDict[currentNum]=1
+    maxV=max(numDict.values())
+    maxV_list=[]
+    for k,v in numDict.items():
+        if v==maxV:
+            maxV_list.append(k)
+    #[k for (k,v) in numDict.items() if v==maxV]
+    return maxV_list
